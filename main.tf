08f0af66161771003943a8bdc6db540cbc8af299
Alan Jones <alanjonesit@outlook.com>
2024-05-04 07:46:10 +0930

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "earth_human" "alan_jones" {
+  tech      = ["AWS", "Terraform", "CI/CD", "GitLab", "GitHub"]
+  certs     = ["AWS Solutions Architect Associate", "Terraform Associate"]
+  interests = ["travel", "being outdoors", "fitness"]
+  
+  goals = {
+    professional = "Drive AWS & IaC innovations, optimize CI/CD processes"
+    personal     = "Develop skills, work collaboratively, teach others"
+    mindset      = "Strive for excellence, continuously improve"
+  }
+}
